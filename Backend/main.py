fe8e2748ef2129b7bc425395c50418d7101704ed
Lakshya Sharma <lakshya.00ls05@gmail.com>
2024-04-04 00:54:30 +0530

    Added Basic Chatbot Component and Connected Backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class Mesaage(BaseModel):
     message : str
 
 app = FastAPI()
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
---
+++ Frontend/src/components/Chatbot.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './Chatbot.css';
+
+const Chatbot = () => {
+  const [message, setMessage] = useState('');
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/chats/1234/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: message }),
+      });
+      let msg = await response.json();
+      console.log(msg.data);
+      
+      if (!response.ok) {
+        throw new Error('Failed to submit message');
+      }
+      
+    
+      setMessage('');
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  return (
+    <div className="chatbot-container">
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={message}
+          onChange={handleMessageChange}
+          className="input-box"
+        />
+        <button onClick={handleSubmit} className="submit-button">Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chatbot;
