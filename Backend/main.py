a2185c1e7f18ae52706baccdb356184eda2aee18
Partha <parthapratimpaul2003@gmail.com>
2024-04-04 00:34:01 +0530

    Created Chat Endpoint for Chat History

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Mesaage(BaseModel):
+    message : str
 
 app = FastAPI()
 
@@ -13,3 +18,9 @@ def search(id: int):
         return {"id": "Ehetsham"}
     else:
         return {"id": "Lakshya"}
+
+@app.post("/chats/{userid}")
+def chat(userid:int,message : Mesaage):
+    print("UserID :",userid)
+    print("Message :", message.message)
+    return {"data": f"received message '{message.message}' from user {userid}"}
